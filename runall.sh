32dceb8ea0eb1b80d97281e16a6809d5795c4b8d
Spencer Dant <shdant113@gmail.com>
2019-11-29 14:23:43 -0500

    fix runall shell script

--- runall.sh
+++ runall.sh
@@ -1,13 +1,17 @@
-rm ghostRatingData.txt
-wget https://webdiplomacy.net/ghostRatingData.txt
-perl GhostRater_ghug_392431984329.pl ghostRatingData.txt Variantsfile.csv RemovedPlayers.csv 510 6 0 1000000 0 `date +%s`
-mv months/* data/Overall/
-perl GhostRater_ghug_392431984329.pl ghostRatingData.txt Variantsfile.csv RemovedPlayers.csv 318 6 0 1000000 0 `date +%s`
-mv months/* data/Gunboat/
-perl GhostRater_ghug_392431984329.pl ghostRatingData.txt Variantsfile.csv RemovedPlayers.csv 82 6 0 1000000 0 `date +%s`
-mv months/* data/Classic/
-perl GhostRater_ghug_392431984329.pl ghostRatingData.txt Variantsfile.csv RemovedPlayers.csv 494 6 0 1000000 0 `date +%s`
-mv months/* data/Live/
-python 1v1_elo_new.py ghostRatingData.txt 6 0 `date +%s`
-mv 1v1/* data/1v1/
+# rm ghostRatingData.txt
+# wget https://webdiplomacy.net/ghostRatingData.txt
+perl gr_processDataDump.pl ghostRatingData.txt Variantsfile.csv RemovedPlayers.csv 510 6 0 1000000 0 `date +%s`
+mv months Overall/
+mv Overall storeGR/
+perl gr_processDataDump.pl ghostRatingData.txt Variantsfile.csv RemovedPlayers.csv 318 6 0 1000000 0 `date +%s`
+mv months Gunboat/
+mv Gunboat storeGR/
+perl gr_processDataDump.pl ghostRatingData.txt Variantsfile.csv RemovedPlayers.csv 82 6 0 1000000 0 `date +%s`
+mv months Classic/
+mv Classic storeGR/
+perl gr_processDataDump.pl ghostRatingData.txt Variantsfile.csv RemovedPlayers.csv 494 6 0 1000000 0 `date +%s`
+mv months Live/
+mv Live/ storeGR/
+python 1v1_elo.py ghostRatingData.txt 6 0 `date +%s`
+mv 1v1 storeGR
 python gr_clean.py
